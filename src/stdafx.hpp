af428e1aef557bda97c66e2be10bf221ce3b87cd
junior <>
2023-03-18 23:56:36 +0100

    Disable errors about deprecated insecure CRT functions for SDK code
     #define _CRT_SECURE_NO_WARNINGS

--- src/stdafx.hpp
+++ src/stdafx.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+#define _CRT_SECURE_NO_WARNINGS
+
 // Start with SDK's precompiled header.
 #define CLIENT_DLL  // remove if making a server plugin
 #include <cbase.h>
+
+#undef _CRT_SECURE_NO_WARNINGS
